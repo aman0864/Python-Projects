8afb31bccc268840d4fc574d61a0565ba73406fa
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-06-04 14:09:14 +0530

    Add files via upload

---
+++ Project 9 (Shop Bill Maker){Basic}/check3.py
@@ -0,0 +1,7 @@
+import os
+location_of_file = str(os.path.dirname(
+    os.path.realpath(__file__)))+"\product_list"
+print(5*"\n")
+print(location_of_file)
+# print(location_of_file)
+print(5*"\n")
